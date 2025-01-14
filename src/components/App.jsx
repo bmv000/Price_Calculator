505c0b68b8915a0c29a5b6924700d2fd87ce67f2
bmv000 <106543741+bmv000@users.noreply.github.com>
2025-01-15 00:45:38 +0200

    added layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import css from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.PC__page}>
+      <form className={css.PC__form}>
+        <h1>Delivery Order Price Calculator</h1>
+        <h2>Details</h2>
+        <label className={css.PC__label}>
+          Venue slug
+          <input className={css.PC__input}></input>
+        </label>
+        <label className={css.PC__label}>
+          Cart value (EUR)
+          <input className={css.PC__input} />
+        </label>
+        <label className={css.PC__label}>
+          User latitude
+          <input className={css.PC__input} />
+        </label>
+        <label className={css.PC__label}>
+          User longitude
+          <input className={css.PC__input} />
+        </label>
+        <button className={css.PC__button} type="submit">
+          Get location
+        </button>
+        <button className={css.PC__button} type="submit">
+          Calculate delivery price
+        </button>
+        <ul>
+          <h2> Price breakdown </h2>
+          <li>Cart Value</li>
+          <li>Delivery fee</li>
+          <li>Delivery distance</li>
+          <li>Small order surcharge</li>
+          <li>Total price</li>
+        </ul>
+      </form>
     </div>
   );
 };
